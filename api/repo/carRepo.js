ce2732cfcef7b987447983bc315ac451df4a179a
khalidhmd <khalidhmd@outlook.com>
2019-01-11 00:03:42 +0200

    add try catch to car repo and optemize updates

--- api/repo/carRepo.js
+++ api/repo/carRepo.js
@@ -1,52 +1,92 @@
 const CarModel = require('../models/car');
 
 const craeteCar = async (car) => {
-  const result = await CarModel.create(car);
+  const result = {};
+  try {
+    result.car = await CarModel.create(car);
+  } catch (err) {
+    result.err = err;
+  }
   return result;
 }
 
 const findCar = async (id) => {
-  const result = await CarModel.findById(id);
+  const result = {};
+  try {
+    result.car = await CarModel.findById(id);
+  } catch (err) {
+    result.err = err;
+  }
   return result;
 }
 
 const addServicedefault = async (carId, servicedefaults) => {
-  const car = await CarModel.findById(carId)
-  car.servicedefaults = [...car.servicedefaults, ...servicedefaults]
-  const result = await car.save();
+  const result = {};
+  try {
+    result.car = await CarModel.findByIdAndUpdate(carId, {
+      $addToSet: { servicedefaults: { $each: servicedefaults }}
+    }, { new: true });
+  } catch (err) {
+    result.err = err;
+  }
   return result;
 }
 
 const deleteServicedefault = async (carId, servicename) => {
-  const car = await CarModel.findById(carId)
-  let servicedefaults = car.servicedefaults.filter( obj => obj.name != servicename);
-  car.servicedefaults = servicedefaults;
-  const result = await car.save();
+  const result = {};
+  try {
+    const car = await CarModel.findById(carId)
+    let servicedefaults = car.servicedefaults.filter( obj => obj.name != servicename);
+    car.servicedefaults = servicedefaults;
+    result.car = await car.save();
+  } catch (err) {
+    result.err = err;
+  }
   return result;
 }
 
 const addServicestatus = async (carId, servicestatuses) => {
-  const car = await CarModel.findById(carId);
-  car.servicestatuses = [...car.servicestatuses, ...servicestatuses];
-  const result = await car.save();
+  const result = {};
+  try {
+    result.car = await CarModel.findByIdAndUpdate(carId, {
+      $addToSet: { servicestatuses: { $each: servicestatuses }}
+    }, { new: true });
+  } catch (err) {
+    result.err = err;
+  }
   return result;
 }
 
 const updateCarkm = async (carId, value) => {
-  const result = await CarModel.findByIdAndUpdate(carId,{ km: value }, { new: true });
+  const result = {};
+  try {
+    result.car = await CarModel.findByIdAndUpdate(carId,{ km: value }, { new: true });
+  } catch (err) {
+    result.err = err;
+  }
   return result;
 }
 
 const deleteServicestatus = async (carId, servicename) => {
-  const car = await CarModel.findById(carId)
-  let servicestatuses = car.servicestatuses.filter( obj => obj.name != servicename);
-  car.servicestatuses = servicestatuses;
-  const result = await car.save();
+  const result = {};
+  try {
+    const car = await CarModel.findById(carId)
+    let servicestatuses = car.servicestatuses.filter( obj => obj.name != servicename);
+    car.servicestatuses = servicestatuses;
+    result.car = await car.save();
+  } catch (err) {
+    result.err = err;
+  }
   return result;
 }
 
 const deleteCar = async (id) => {
-  await CarModel.findByIdAndRemove(id);
+  const result = {};
+  try {
+    await CarModel.findByIdAndRemove(id);
+  } catch (err) {
+    result.err = err;
+  }
 }
 
 module.exports.craeteCar = craeteCar;
--- test/car_test.js
+++ test/car_test.js
@@ -12,46 +12,54 @@ describe('Testing car Repo repo', function() {
   it('Saves user car to DB', async function() {
     const car = new CarModel({make:'Hundai', model: 'Verna'});
     a = await carRepo.craeteCar(car);
-    assert(a.id == car.id);
-    id = a.id;
+    assert(a.err == null);
+    assert(a.car.id == car.id);
+    id = a.car.id;
   });
 
   it('Reads user car form DB', async function() {
     const b = await carRepo.findCar(id);
-    assert(b.id == id);
+    assert(b.err == null);
+    assert(b.car.id == id);
   });
 
   it('Updates user car millage (km) in DB', async function () {
     const g = await carRepo.updateCarkm(id, 55);
-    assert(g.km == 55);
+    assert(g.car.km == 55);
+    assert(g.err == null);
   });
   
   it('Adds servicedefaults to user car in DB', async function () {  
     const c = await carRepo.addServicedefault(id, [{name: 'تغيير زيت'}, {name: 'تغيير دورة تبريد'}]);
-    assert(c.servicedefaults[0].name == 'تغيير زيت');
-    assert(c.servicedefaults[0].km == 10)
+    assert(c.car.servicedefaults[0].name == 'تغيير زيت');
+    assert(c.car.servicedefaults[0].km == 10)
+    assert(c.err == null);
   });
 
   it('Deletes servicedefault from user car in DB', async function () {
     const d = await carRepo.deleteServicedefault(id, "تغيير زيت");
-    assert(d.servicedefaults.length == 1);
+    assert(d.car.servicedefaults.length == 1);
+    assert(d.err == null);
   });
 
   it('Adds servicestatus to user car in DB', async function () {  
     const e = await carRepo.addServicestatus(id, [{name: 'تغيير زيت', nextkm: 20}]);
-    assert(e.servicestatuses[0].name == 'تغيير زيت');
-    assert(e.servicestatuses[0].nextkm == 20)
+    assert(e.car.servicestatuses[0].name == 'تغيير زيت');
+    assert(e.car.servicestatuses[0].nextkm == 20);
+    assert(e.err == null);
   });
 
   it('Deletes servicestatus from user car in DB', async function () {
     const f = await carRepo.deleteServicestatus(id, "تغيير زيت");
-    assert(f.servicestatuses.length == 0);
+    assert(f.car.servicestatuses.length == 0);
+    assert(f.err == null);
   });
   
   it('Deletes user car from DB', async function () {
     await carRepo.deleteCar(id);
-    const car = await carRepo.findCar(id);
-    assert(car == null);
+    const result = await carRepo.findCar(id);
+    assert(result.car == null);
+    assert(result.err == null);
   });
 });
 
